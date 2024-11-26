c9cf72c517a4d2c126ac27cf565a73f63bd4153d
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-26 05:03:03 +0000

    nodejs

--- 1.js
+++ 1.js
@@ -1,7 +1,8 @@
 const read=require("readline-sync")
-let age1=read.question("Welcome to age checker")
-console.log(age1);
+console.log("Welcome to age checker");
+
 
 let age=read.question("Enter your age: ")
 console.log(`Your age is ${age}`);
+
 
