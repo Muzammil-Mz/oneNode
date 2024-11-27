b9a15223f78563210bf57ec1d1a0ddc23bcb0a30
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-27 15:35:57 +0000

    27

--- 1.js
+++ 1.js
@@ -1,10 +1,13 @@
+// const read=require("readline-sync")
+// console.log("Welcome to age checker");
+
+
+// let age=read.question("Enter your age: ")
+// console.log(`Your age is ${age}`);
+
+// console.log("thank you");
+
+
+
 const read=require("readline-sync")
-console.log("Welcome to age checker");
-
-
-let age=read.question("Enter your age: ")
-console.log(`Your age is ${age}`);
-
-console.log("thank you");
-
-
+console.log("yessss");
